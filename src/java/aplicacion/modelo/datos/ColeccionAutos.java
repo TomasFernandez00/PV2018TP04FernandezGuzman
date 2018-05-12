6e94a90a8e5444d072c06f5c71bf4e1d37eafe68
TomasFernandez <laalaraca@gmail.com>
2018-05-12 15:40:56 -0300

    Agregado punto 5

---
+++ src/java/aplicacion/modelo/datos/ColeccionAutos.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.modelo.datos;
+
+import aplicacion.modelo.dominio.Auto;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tomas
+ */
+public class ColeccionAutos implements Serializable {
+
+	private ArrayList<Auto> autos;
+
+	public ColeccionAutos() {
+		autos = new ArrayList();
+	}
+
+	public void agregarAutos(Auto unAuto) {
+		getAutos().add(unAuto);
+	}
+
+	public void modificarAutos(Auto unAuto) {
+		for (Auto a : autos) {
+			if (a.getPatente().equals(unAuto.getPatente())) {
+				getAutos().set(autos.indexOf(a), unAuto);
+				break;
+			}
+		}
+	}
+
+	public void eliminarAutos(Auto unAuto) {
+		for (Auto a : autos) {
+			if (a.getPatente().equals(unAuto.getPatente())) {
+				autos.remove(autos.indexOf(a));
+				break;
+			}
+		}
+	}
+
+	/**
+	 * @return the autos
+	 */
+	public ArrayList<Auto> getAutos() {
+		return autos;
+	}
+
+	/**
+	 * @param autos the autos to set
+	 */
+	public void setAutos(ArrayList<Auto> autos) {
+		this.autos = autos;
+	}
+}
